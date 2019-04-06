63293e93803d7bd8cdfdff38195345259b153e0e
Geniusmicheal <33321629+Geniusmicheal@users.noreply.github.com>
2019-04-06 06:33:44 +0100

    Division Implement
    Implement integer division without using the division operator.

--- divide.php
+++ divide.php
@@ -12,40 +12,38 @@
 	if (isset($_POST['Ok'])) {
 		$num1=$_POST['num1'];
 		$num2=$_POST['num2'];
-		if($num1 < 0 && $num2 < 0)
-		{
-			// $positive = abs($value);
-			$x =1;
-			do {
-				$r=abs($num2) * $x;
-				$x ++;
-			}while ($r <= abs($num1));
-			// echo "Highest Factor = ".$r-=abs($num2);
-			echo "<br> Quotient = ".$x-=2;
-			echo "<br> Remainder = ".$num1%$num2;
+		function division($numerator, $denominator){
+			if($numerator< 0 && $denominator < 0)
+			{
+				$x =1;
+				do {
+					$r=abs($denominator) * $x;
+					$x ++;
+				}while ($r <= abs($numerator));
+				echo "<br> Quotient = ".$x-=2;
+				echo "<br> Remainder = ".-($numerator%$denominator);
+			}else if($numerator< 0 || $denominator < 0)
+			{
+				$x =1;
+				do {
+					$r=abs($denominator) * $x;
+					$x ++;
+				}while ($r <= abs($numerator));
+				echo "<br> Quotient = -".$x-=2;
+				echo "<br> Remainder = ".$numerator%$denominator;
+			}else if($numerator == 0 || $denominator == 0){
+				echo "Input error";
+			}else{
+				$x =1;
+				do {
+					$r=$denominator * $x;
+					$x ++;
+				}while ($r <= $numerator);
+				echo "Highest Factor = ".$r-=abs($denominator);
+				echo "<br> Quotient = ".$x-=2;
+				echo "<br> Remainder = ".$numerator%$denominator;
+			}
 		}
-		else if($num1 < 0 || $num2 < 0)
-		{
-			$x =1;
-			do {
-				$r=abs($num2) * $x;
-				$x ++;
-			}while ($r <= abs($num1));
-			// echo "Highest Factor = ".$r-=abs($num2);
-			echo "<br> Quotient = "."-".$x-=2;
-			echo "<br> Remainder = ".$num1%$num2;
-		}else if($num1 == 0 && $num2 == 0){
-
-		}else{
-			$x =1;
-			do {
-				$r=$num2 * $x;
-				$x ++;
-			}while ($r <= $num1);
-			echo "Highest Factor = ".$r-=$num2;
-			echo "<br> Quotient = ".$x-=2;
-			echo "<br> Remainder = ".$num1%$num2;
-		}
-
+		division($num1,$num2);
 	}
 ?>
