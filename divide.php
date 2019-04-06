a93bfab3f4e7e5d8e4d8d747005e2348d12214e2
Geniusmicheal <33321629+Geniusmicheal@users.noreply.github.com>
2019-04-06 05:30:29 +0100

    Division Homework
    Implement integer division without using the division operator

--- divide.php
+++ divide.php
@@ -1,37 +1,49 @@
 <html>
-<body>
-<form method="POST"> 
-Numerator <input type="number" name="num1" require>
-Denominator<input type="number" name="num2" require><br><br> 
-Result: <input type="submit" name="Ok">
-</form>
-</body>
+	<body>
+		<form method="POST"> 
+		Numerator <input type="number" name="num1" require>
+		Denominator<input type="number" name="num2" require><br><br> 
+		Result: <input type="submit" name="Ok">
+		</form>
+	</body>
 </html>
 
 <?php 
-if (isset($_POST['Ok'])) {
-  $num1=$_POST['num1'];
-  $num2=$_POST['num2'];
-    $x =1;
-   
-    do {
-      $r=$num2 * $x;
-      $x ++;
+	if (isset($_POST['Ok'])) {
+		$num1=$_POST['num1'];
+		$num2=$_POST['num2'];
+		if($num1 < 0 && $num2 < 0)
+		{
+			// $positive = abs($value);
+			$x =1;
+			do {
+				$r=abs($num2) * $x;
+				$x ++;
+			}while ($r <= abs($num1));
+			// echo "Highest Factor = ".$r-=abs($num2);
+			echo "<br> Quotient = ".$x-=2;
+			echo "<br> Remainder = ".$num1%$num2;
+		}
+		else if($num1 < 0 || $num2 < 0)
+		{
+			$x =1;
+			do {
+				$r=abs($num2) * $x;
+				$x ++;
+			}while ($r <= abs($num1));
+			// echo "Highest Factor = ".$r-=abs($num2);
+			echo "<br> Quotient = "."-".$x-=2;
+			echo "<br> Remainder = ".$num1%$num2;
+		}else{
+			$x =1;
+			do {
+				$r=$num2 * $x;
+				$x ++;
+			}while ($r <= $num1);
+			echo "Highest Factor = ".$r-=$num2;
+			echo "<br> Quotient = ".$x-=2;
+			echo "<br> Remainder = ".$num1%$num2;
+		}
 
-    } while ($r <= $num1);
-    echo "Highest Factor = ".$r-=$num2;
-    echo "<br> Quotient = ".$x-=2;
-    echo "<br> Remainder = ".$num1%$num2;
-
-  }
-  // do {
-  //   $r=$num1 * $x;
-  //   $x++;
-
-  // } while ($r <= $num1);
-  // echo "Highest Factor = ".$r-=$num2;
-  // echo "<br> Quotient = ".$x-=1;
-  // echo "<br> Remainder = ".$num1%$num2;
-
-}
+	}
 ?>
